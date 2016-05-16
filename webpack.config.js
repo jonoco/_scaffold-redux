11fde07bac1e94c5e01dbc9122275f3ef9f3347d
Joshua Cox <joshua.n.cox@gmail.com>
2016-05-15 21:39:26 -0700

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { exclude: /node_modules/, 
+        test: /\.js?$/, 
+        loader: 'babel' },
+      { test: /\.jsx?$/, 
+        loaders: ['react-hot', 'babel'], 
+        include: path.join(__dirname,'src')
+      }
+    ],
+    query: { presets: ['es2015', 'react', 'stage-2'] }
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
