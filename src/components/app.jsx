915bf109deceb3b76da4dfddefddf4ba61f6c007
Joshua Cox <joshua.n.cox@gmail.com>
2016-06-19 22:24:56 -0700

    update:docs docs more descriptive

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <div className='app'>
-      	<h1>Redux scaffold</h1>
+      	{this.props.children}
       </div>
     );
   }
 }
+
+export default App;
